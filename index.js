4ed917c904b4b46f8f0acac43921545b6fd76afa
ukarinekids <100566427+ukarinekids@users.noreply.github.com>
2022-02-28 18:01:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Please donate to Ukarine :  ETH Address 0xAD4E64Fb9A24716231c3272d906644C333Bf44D8)'
+  document.getElementById('main-content').innerHTML = 'Please donate to the kids in Ukarine :  ETH Address 0xAD4E64Fb9A24716231c3272d906644C333Bf44D8)'
 }
