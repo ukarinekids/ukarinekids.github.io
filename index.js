08495d84004cc4b179d9479168b99cfb923d757b
ukarinekids <100566427+ukarinekids@users.noreply.github.com>
2022-02-28 18:17:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Due to war from Russia, the kids in Ukarine are now suffering homelessness and hunger.   Please donate to the kids in Ukarine :  BTC Address 34oxFKCUe92zMu8U5XazWQF6i9EYU5auRo)'
+  document.getElementById('main-content').innerHTML = 'Due to war from Russia, the kids in Ukarine are now suffering homelessness and hunger.   Please donate to the kids in Ukarine :  BTC Address 34oxFKCUe92zMu8U5XazWQF6i9EYU5auRo'
 }
